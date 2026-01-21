28cd6c4888f17cad91caa2a94206d1199f0d914c
Abhish7k <abhishekbhosale.dev@gmail.com>
2026-01-21 16:57:00 +0530

    feat: admin dashboard stat cards
    added stat cards on the admin dashboard

--- frontend/src/pages/admin/AdminDashboard.tsx
+++ frontend/src/pages/admin/AdminDashboard.tsx
@@ -1,5 +1,20 @@
+import AdminDashboardStatsGrid from "@/components/dashboards/admin/AdminDashboardStatsGrid";
+
 const AdminDashboard = () => {
-  return <div>AdminDashboard</div>;
+  return (
+    <div>
+      {/* header */}
+      <div className="mb-10">
+        <h1 className="text-lg font-semibold">Admin Dashboard</h1>
+        <p className="text-sm text-muted-foreground">
+          Platform-level overview and system management
+        </p>
+      </div>
+
+      {/*  */}
+      <AdminDashboardStatsGrid />
+    </div>
+  );
 };
 
 export default AdminDashboard;
